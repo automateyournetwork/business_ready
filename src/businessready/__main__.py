16ebedf447a70a23d57309c1621fb46d7b1c7949
John Capobianco <ptcapo@gmail.com>
2022-02-09 13:23:27 -0500

    interface

--- src/businessready/__main__.py
+++ src/businessready/__main__.py
@@ -1,7 +1,8 @@
 import sys
-from .brd import IOS_learned_acl,IOS_learned_arp,IOS_show_ip_interface_brief,DNAC_Sites
+from .brd import IOS_learned_acl,IOS_learned_arp,IOS_learned_interface,IOS_show_ip_interface_brief,DNAC_Sites
 if __name__ == "__main__":
     print(IOS_learned_acl(sys.argv[1]))
     print(IOS_learned_arp(sys.argv[1]))
-    print(IOS_show_ip_interface_brief(sys.argv[1]))
+    print(IOS_learned_arp(sys.argv[1]))
+    print(IOS_learned_interface(sys.argv[1]))
     print(DNAC_Sites(sys.argv[1]))
