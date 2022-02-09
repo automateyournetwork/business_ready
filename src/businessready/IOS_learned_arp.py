4a81750be83a63bdbe2185ee29e850cf598e6473
John Capobianco <ptcapo@gmail.com>
2022-02-09 14:47:01 -0500

    move to ubuntu

--- src/businessready/IOS_learned_arp.py
+++ src/businessready/IOS_learned_arp.py
@@ -1,4 +1,4 @@
 import sys
 from .brd import IOS_learned_arp
 def run():
-    print(IOS_learned_arp(sys.argv[1]))
+    print(IOS_learned_arp(*sys.argv[1:3]))
--- src/businessready/__main__.py
+++ src/businessready/__main__.py
@@ -1,8 +1,8 @@
-import sys
-from .brd import IOS_learned_acl,IOS_learned_arp,IOS_learned_interface,IOS_show_ip_interface_brief,DNAC_Sites
-if __name__ == "__main__":
-    print(IOS_learned_acl(sys.argv[1]))
-    print(IOS_learned_arp(sys.argv[1]))
-    print(IOS_learned_interface(sys.argv[1]))
-    print(IOS_show_ip_interface_brief(sys.argv[1]))
-    print(DNAC_Sites(sys.argv[1]))
+# import sys
+# from .brd import IOS_learned_acl,IOS_learned_arp,IOS_learned_interface,IOS_show_ip_interface_brief,DNAC_Sites
+# if __name__ == "__main__":
+#     print(IOS_learned_acl(sys.argv[4]))
+#     print(IOS_learned_arp(sys.argv[4]))
+#     print(IOS_learned_interface(sys.argv[4]))
+#     print(IOS_show_ip_interface_brief(sys.argv[4]))
+#     print(DNAC_Sites(sys.argv[2]))
