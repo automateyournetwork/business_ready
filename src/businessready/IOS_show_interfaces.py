cb89618d43ad34e2d7dd437f66e0f21fcf4a0d70
John Capobianco <ptcapo@gmail.com>
2022-02-12 16:13:20 -0500

    show etherchannel summary

---
+++ src/businessready/IOS_show_interfaces.py
@@ -0,0 +1,4 @@
+# import sys
+# from .brd import IOS_show_interfaces
+# def run():
+#     print(IOS_show_interfaces(sys.argv[1]))
--- src/businessready/brd.py
+++ src/businessready/brd.py
@@ -1224,7 +1224,7 @@ def IOS_show_environment_all(hostname, username, password, ip):
         for device in new_testbed:
             device.connect()
 
-        # Show CDP Neighbors to JSON
+        # Show Environment All to JSON
 
             try:
                 show_environment_all = device.parse("show environment all")
@@ -1292,7 +1292,7 @@ def IOS_show_etherchannel_summary(hostname, username, password, ip):
         for device in new_testbed:
             device.connect()
 
-        # Show CDP Neighbors to JSON
+        # Show Etherchannel Summary to JSON
 
             try:
                 show_etherchannel_summary = device.parse("show etherchannel summary")
@@ -1344,6 +1344,74 @@ def IOS_show_etherchannel_summary(hostname, username, password, ip):
                         fh.write(parsed_output)               
                         fh.close()                        
         return(show_etherchannel_summary)
+    except Exception as e:
+        logging.exception(e)
+
+def IOS_show_interfaces(hostname, username, password, ip):
+    try:
+    # Create Testbed
+        filename = hostname
+        first_testbed = Testbed('dynamicallyCreatedTestbed')
+        testbed_device = Device(hostname,
+                    alias = hostname,
+                    type = 'switch',
+                    os = 'iosxe',
+                    credentials = {
+                        'default': {
+                            'username': username,
+                            'password': password,
+                        }
+                    },
+                    connections = {
+                        'cli': {
+                            'protocol': 'ssh',
+                            'ip': ip,
+                            'port': 22,
+                            'arguements': {
+                                'connection_timeout': 360
+                            }
+                        }
+                    })
+        testbed_device.testbed = first_testbed
+        new_testbed = testbed.load(first_testbed)
+        # ---------------------------------------
+        # Loop over devices
+        # ---------------------------------------
+        for device in new_testbed:
+            device.connect()
+
+        # Show Interfaces to JSON
+
+            try:
+                show_interfaces = device.parse("show interfaces")
+            except:
+                show_interfaces = f"{ hostname } Can't Parse"
+
+        # Pass to template 
+
+        if show_interfaces != f"{ hostname } Can't Parse":
+            IOS_show_interfaces_template = env.get_template('IOS_show_interfaces.j2')
+            loop_counter = 0
+        # Render Templates
+            for filetype in filetype_loop:
+                parsed_output = IOS_show_interfaces_template.render(to_parse_interfaces=show_interfaces,filetype_loop=loop_counter)
+                loop_counter = loop_counter + 1
+
+    # -------------------------
+    # Save the files
+    # -------------------------
+                if loop_counter <= 3:
+                    with open(f"{ filename }_Show Interfaces.{ filetype }", "w") as fh:
+                        fh.write(parsed_output)               
+                        fh.close()
+                else:
+                    with open(f"{ filename }_Show Interfaces Mind Map.md", "w") as fh:
+                        fh.write(parsed_output)               
+                        fh.close()
+                with open(f"{ filename }_Show Interfaces.json", "w") as fh:
+                    json.dump(show_interfaces, fh, indent=4, sort_keys=True)
+                    fh.close()                                 
+        return(show_interfaces)
     except Exception as e:
         logging.exception(e)
 
