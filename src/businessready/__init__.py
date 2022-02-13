3e9a328edfc306c87c0500ac276f2a12b444ecf8
John Capobianco <ptcapo@gmail.com>
2022-02-12 19:02:33 -0500

    show int status

--- src/businessready/__init__.py
+++ src/businessready/__init__.py
@@ -1,2 +1,2 @@
-__version__ = '0.3.72'
+__version__ = '0.3.74'
 from .brd import *
--- src/businessready/brd.py
+++ src/businessready/brd.py
@@ -1415,6 +1415,74 @@ def IOS_show_interfaces(hostname, username, password, ip):
     except Exception as e:
         logging.exception(e)
 
+def IOS_show_interfaces_status(hostname, username, password, ip):
+    try:
+    # Create Testbed
+        filename = hostname
+        first_testbed = Testbed('dynamicallyCreatedTestbed')
+        testbed_device = Device(hostname,
+                    alias = hostname,
+                    type = 'switch',
+                    os = 'iosxe',
+                    credentials = {
+                        'default': {
+                            'username': username,
+                            'password': password,
+                        }
+                    },
+                    connections = {
+                        'cli': {
+                            'protocol': 'ssh',
+                            'ip': ip,
+                            'port': 22,
+                            'arguements': {
+                                'connection_timeout': 360
+                            }
+                        }
+                    })
+        testbed_device.testbed = first_testbed
+        new_testbed = testbed.load(first_testbed)
+        # ---------------------------------------
+        # Loop over devices
+        # ---------------------------------------
+        for device in new_testbed:
+            device.connect()
+
+        # Show Interfaces to JSON
+
+            try:
+                show_interfaces_status = device.parse("show interfaces status")
+            except:
+                show_interfaces_status = f"{ hostname } Can't Parse"
+
+        # Pass to template 
+
+        if show_interfaces_status != f"{ hostname } Can't Parse":
+            IOS_show_interfaces_status_template = env.get_template('IOS_show_interfaces_status.j2')
+            loop_counter = 0
+        # Render Templates
+            for filetype in filetype_loop:
+                parsed_output = IOS_show_interfaces_status_template.render(to_parse_interfaces=show_interfaces_status['interfaces'],filetype_loop=loop_counter)
+                loop_counter = loop_counter + 1
+
+    # -------------------------
+    # Save the files
+    # -------------------------
+                if loop_counter <= 3:
+                    with open(f"{ filename }_Show Interfaces Status.{ filetype }", "w") as fh:
+                        fh.write(parsed_output)               
+                        fh.close()
+                else:
+                    with open(f"{ filename }_Show Interfaces Status Mind Map.md", "w") as fh:
+                        fh.write(parsed_output)               
+                        fh.close()
+                with open(f"{ filename }_Show Interfaces Status.json", "w") as fh:
+                    json.dump(show_interfaces_status, fh, indent=4, sort_keys=True)
+                    fh.close()                                 
+        return(show_interfaces_status)
+    except Exception as e:
+        logging.exception(e)
+
 def IOS_show_ip_interface_brief(hostname, username, password, ip):
     try:
     # Create Testbed
